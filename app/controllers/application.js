f26c918367c1c67fe5f29c016186eed19ea1a0c3
Chris Thoburn <runspired@gmail.com>
2014-10-30 17:32:01 -0500

    initial ember-cli commit

---
+++ app/controllers/application.js
@@ -0,0 +1,8 @@
+import Ember from "ember";
+
+export default Ember.Controller.extend({
+  session : {
+    user : {},
+    token : '_fake_token_'
+  }
+});
---
+++ app/fixtures/FIXTURES.js
@@ -0,0 +1,48 @@
+var FIXTURES = {
+    users : [
+        {
+            id : 1,
+            name : 'Chris Thoburn',
+            photo : '../fixtures/photos/cthoburn.jpg',
+            email : 'me@uchicago.edu'
+        }
+    ],
+    ideas : [
+        {
+            id : 1,
+            title : '',
+            description : '',
+            category : '',
+            location : {
+                name : '',
+                latitude : '',
+                longitude : '',
+                address : {
+                    street : '',
+                    city : '',
+                    state : '',
+                    zipcode : ''
+                }
+            },
+            status : ''
+        },
+        {
+            id : 1,
+            title : '',
+            description : '',
+            category : '',
+            location : {
+                name : '',
+                latitude : '',
+                longitude : '',
+                address : {
+                    street : '',
+                    city : '',
+                    state : '',
+                    zipcode : ''
+                }
+            },
+            status : ''
+        }
+    ]
+};
